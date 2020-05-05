bbc7cb0d7f301febd8cd87623b05476aab052813
Richard J Payton <richard.payton@lyteworx.com>
2020-05-05 13:17:13 -0400

    Create gen_index.py
    This script make the index.html file based on the contents of the charts directory.

---
+++ gen_index.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+html = """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>COVID-19 Analysis</title>
+</head>
+<body>
+<ul>
+"""
+
+for p in Path('charts').glob('*.html'):
+    name = ' '.join(p.stem.split('_'))
+    html += f"""    <li><a href="{p.as_posix()}">{name.title()}</a></li>\n"""
+
+html += """</ul>
+</body>
+</html>
+"""
+
+with open('index.html', 'w') as f:
+    f.write(html)
